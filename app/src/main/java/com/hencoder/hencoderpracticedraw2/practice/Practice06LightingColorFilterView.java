acdb674d518aae028462c874868d1e8fe22181be
OCD <conghaohu0711@outlook.com>
2020-01-28 14:11:26 +0800

    update

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice06LightingColorFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice06LightingColorFilterView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -38,9 +41,15 @@ public class Practice06LightingColorFilterView extends View {
         // 使用 Paint.setColorFilter() 来设置 LightingColorFilter
 
         // 第一个 LightingColorFilter：去掉红色部分
+        ColorFilter colorFilterRemoveR = new LightingColorFilter(0x00ffff, 0x000000);
+        paint.setColorFilter(colorFilterRemoveR);
         canvas.drawBitmap(bitmap, 0, 0, paint);
+        paint.setColorFilter(null);
 
         // 第二个 LightingColorFilter：增强绿色部分
+        ColorFilter colorFilterAddG = new LightingColorFilter(0xffffff, 0x003000);
+        paint.setColorFilter(colorFilterAddG);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 100, 0, paint);
+        paint.setColorFilter(null);
     }
 }
